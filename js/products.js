34d48fd97a52d9121c13a22fe1a210ac85d93d42
magdyZiad <magdyziad.dev@gmail.com>
2026-06-28 16:26:08 +0300

    add products.js

---
+++ js/products.js
@@ -0,0 +1,77 @@
+let slider = document.getElementById("slider")
+
+let products = [
+    {
+        name: "Modern Desk Lamp",
+        img: "Images/slider-1.png",
+        price: 30.00,
+        reviews: 128,
+    },
+    {
+        name: "Smart Watch",
+        img: "Images/slider-2.png",
+        price: 200.00,
+        reviews: 600,
+    },
+    {
+        name: "Wall Clock",
+        img: "Images/slider-3.png",
+        price: 80.00,
+        reviews: 800,
+    },
+    {
+        name: "Laptop",
+        img: "Images/slider-4.png",
+        price: 3000.00,
+        reviews: 3000,
+    },
+    {
+        name: "iPhone 16 Pro Max",
+        img: "Images/slider-5.webp",
+        price: 8000.00,
+        reviews: 3000,
+    },
+    {
+        name: "Air Pods 4",
+        img: "Images/slider-6.webp",
+        price: 1000.00,
+        reviews: 2000,
+    },
+]
+
+slider.innerHTML = ""
+products.forEach((product) => {
+    slider.innerHTML += `
+            <div class="flex flex-col mx-2 relative group group-hover:translate-y-0 group-hover:opacity-100">
+                <button
+                    class="opacity-0 duration-300 translate-y-8 group-hover:translate-y-0 group-hover:opacity-100 g absolute z-50 left-2 right-2 py-2 top-[67%] cursor-pointer text-white bg-blue-700 font-semibold rounded-md">Add
+                    To Cart
+                </button>
+                <div class="relative">
+                    <img src="${product.img}" alt="" class="rounded-lg">
+                    <div
+                        class="absolute right-[10px] top-[10px] w-[35px] h-[35px] text-[22px] flex justify-center items-center bg-white rounded-full  cursor-pointer">
+                        <i class="fa-solid fa-heart "></i>
+                    </div>
+                </div>
+                <div class="flex flex-col gap-1 mt-2">
+                    <h3 class="text-xl font-semibold">${product.name}</h3>
+                    <div class="flex items-center gap-2">
+                        <div class="flex text-sm">
+                            <i class="fa-solid fa-star text-yellow-400"></i>
+                            <i class="fa-solid fa-star text-yellow-400"></i>
+                            <i class="fa-solid fa-star text-yellow-400"></i>
+                            <i class="fa-solid fa-star text-yellow-400"></i>
+                            <i class="fa-solid fa-star-half-stroke text-yellow-400"></i>
+                        </div>
+                        <span class="text-gray-500">(${product.reviews})</span>
+                    </div>
+                    <span class="text-blue-500 font-semibold">$${product.price.toFixed(2)}</span>
+                    <button
+                    class="lg:hidden py-2 cursor-pointer text-white bg-blue-700 font-semibold rounded-md">Add
+                    To Cart
+                </button>
+                </div>
+            </div>
+    `
+})
