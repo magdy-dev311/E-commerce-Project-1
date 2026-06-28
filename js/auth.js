ec8b8b1751ea7667b3881cb3666522504f715e34
magdyZiad <magdyziad.dev@gmail.com>
2026-06-28 15:30:58 +0300

    finish the Logic of Login - Signup.

--- js/auth.js
+++ js/auth.js
@@ -14,23 +14,64 @@ let signupFullNameInput = document.getElementById("signup-full-name");
 let signupEmailInput = document.getElementById("signup-email");
 let signupPasswordInput = document.getElementById("signup-password");
 
-signupForm.classList.add("hidden")
+signupForm.classList.add("hidden");
 
-toggleToSignup.addEventListener("click", ()=>{
-    loginForm.classList.add("hidden")
-    signupForm.classList.remove("hidden")
-})
+toggleToSignup.addEventListener("click", () => {
+    loginForm.classList.add("hidden");
+    signupForm.classList.remove("hidden");
+});
 
-toggleToLogin.addEventListener("click", ()=>{
-    signupForm.classList.add("hidden")
-    loginForm.classList.remove("hidden")
-})
+toggleToLogin.addEventListener("click", () => {
+    signupForm.classList.add("hidden");
+    loginForm.classList.remove("hidden");
+});
 
-let users = JSON.parse(window.localStorage.getItem("users")) || []
+let users = JSON.parse(window.localStorage.getItem("users")) || [];
 
 signupForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let name = signupFullNameInput.value;
+    let email = signupEmailInput.value;
+    let password = signupPasswordInput.value;
+
+    console.log(name);
+    console.log(email);
+    console.log(password);
+
+    existingUser = users.find((user) => user.email === email);
+
+    if (!existingUser) {
+        let newUser = { name, email, password };
+        users.push(newUser);
+        localStorage.setItem("users", JSON.stringify(users));
+        signupFullNameInput.value = "";
+        signupEmailInput.value = "";
+        signupPasswordInput.value = "";
+        alert("Sign Up Done");
+        signupForm.classList.add("hidden");
+        loginForm.classList.remove("hidden");
+    } else {
+        alert("Email Is Already Exist");
+    }
+});
+
+loginForm.addEventListener("submit", (e) => {
     e.preventDefault()
+    let email = loginEmailInput.value;
+    let password = loginPasswordInput.value;
+    let user = users.find((user) => user.email === email);
 
-    
+    if (user) {
+        if (user.password === password) {
+            localStorage.setItem("user", JSON.stringify(user))
+            alert("Login Successfully")
+            window.location.href="index.html"
+        } else {
+            alert("Email or Password Incorrect")
+        }
+    } else {
+        alert("User Does not Exist")
+    }
 
-})
+});
